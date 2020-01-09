17a29317588a4d23d4665449d6520d2e840c91d6
Gabe Valdivia <gabe@gabeswebcoding.com>
2020-01-08 22:01:28 -0800

    Retrieve and render WP site title and tagline, apply inner wrapper to menu area. Set to max width of 960px, use flexbox

--- my-portfolio/src/components/MainMenu.js
+++ my-portfolio/src/components/MainMenu.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import {graphql, StaticQuery, Link } from 'gatsby'
 import styled from 'styled-components'
+import SiteInfo from './Siteinfo'
 
 const MainMenuWrapper = styled.div`
     display: flex;
     background-color: rgb(3, 27, 77);
+`
+
+const MainMenuInner = styled.div`
+    max-width: 960px;
+    margin: 0 auto;
+    display: flex;
+    width: 960px;
+    height: 100%;
 `
 
 const MenuItem = styled(Link)`
@@ -15,35 +24,40 @@ const MenuItem = styled(Link)`
 
 export const MainMenu = () => {
     return (
-        <StaticQuery query={graphql`
-        {
-        allWordpressWpApiMenusMenusItems(filter: {
-            name: {
-            eq: "Main Menu"
-            }
-        }) {
-            edges{
-            node {
-                name
-                items {
-                title
-                object_slug
+      <StaticQuery
+        query={graphql`
+          {
+            allWordpressWpApiMenusMenusItems(
+              filter: { name: { eq: "Main Menu" } }
+            ) {
+              edges {
+                node {
+                  name
+                  items {
+                    title
+                    object_slug
+                  }
                 }
+              }
             }
-            }
-        }
-        }        
-        `} render={props => (
-            <MainMenuWrapper>
-                {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(item => (
-                    <MenuItem to={item.object_slug} key={item.title} >
+          }
+        `}
+        render={props => (
+          <MainMenuWrapper>
+            <MainMenuInner>
+                <SiteInfo />
+                {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(
+                    item => (
+                    <MenuItem to={item.object_slug} key={item.title}>
                         {item.title}
                     </MenuItem>
-                ))}
-            </MainMenuWrapper>
-        )} />    
-        
-    );
+                    )
+                )}
+            </MainMenuInner>
+          </MainMenuWrapper>
+        )}
+      />
+    )
 }
 
 export default MainMenu
