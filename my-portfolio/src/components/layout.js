202a3bd1ec40068672ec2f794bfdb136b44c5267
Gabe Valdivia <gabe@gabeswebcoding.com>
2020-01-08 21:07:19 -0800

    Inital commit, connected to WP backend, pulling in main menu, slug, title and content, basic setup for styled-components

---
+++ my-portfolio/src/components/layout.js
@@ -0,0 +1,33 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import MainMenu from "./MainMenu"
+
+import Header from "./header"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+
+  body{
+    font-family: 'Open Sans', sans-serif;
+  }
+`
+
+
+const Layout = ({ children }) => (
+  <div>
+    <GlobalStyles />
+    <Header />
+    <MainMenu />
+    {children}
+  </div>
+)
+
+
+export default Layout
